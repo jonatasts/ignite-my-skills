b3d58889c2343a14b5a70ece00c2ee487196c27a
Jonatas Santos <jonatassantos96@hotmail.com>
2022-07-13 23:34:09 -0300

    feat: Add typescript on button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,10 +3,16 @@ import { Text } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 
 import styles from "./styles";
+import { ButtonProps } from "./types";
 
-export const Button = ({ title, onPress }) => {
+export const Button = ({ title, onPress, ...rest }: ButtonProps) => {
   return (
-    <RectButton style={styles.button} activeOpacity={0.3} onPress={onPress}>
+    <RectButton
+      style={styles.button}
+      activeOpacity={0.3}
+      onPress={onPress}
+      {...rest}
+    >
       <Text style={styles.buttonText}>{title}</Text>
     </RectButton>
   );
---
+++ src/components/Button/types.ts
@@ -0,0 +1,5 @@
+import { RectButtonProps } from "react-native-gesture-handler";
+
+export type ButtonProps = {
+  title: string;
+} & RectButtonProps;
