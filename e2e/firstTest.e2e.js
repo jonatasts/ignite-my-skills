8492954593a4c0ace9e0de44fc4451e8ddc9a38b
jonatasts <jonatassantos96@hotmail.com>
2023-04-12 20:24:56 -0300

    🧪 Add tests e2e

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,4 +1,4 @@
-describe("Meus primeiros testes", () => {
+describe("Home Screen", () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -9,5 +9,18 @@ describe("Meus primeiros testes", () => {
 
   it("should have welcome screen", async () => {
     await expect(element(by.id("welcome"))).toBeVisible();
+  });
+
+  it("check register a new skill", async () => {
+    const inputNewSkill = await element(by.id("new-skill-input"));
+    const buttonNewSkill = await element(by.id("new-skill-button"));
+
+    await inputNewSkill.tap();
+    await inputNewSkill.typeText("React Native\n");
+    await buttonNewSkill.tap();
+
+    await expect(element(by.id("flat-list-skills"))).toBeVisible();
+
+    await expect(element(by.id("React Native"))).toBeVisible();
   });
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -47,32 +47,46 @@ export const Home = () => {
     <View style={styles.container}>
       <SafeAreaView />
 
-      <Text style={styles.title}>Welcome, Jhon</Text>
+      <Text testID="welcome" style={styles.title}>
+        Welcome, Jhon
+      </Text>
 
       <Text style={styles.greetings}>{greeting}</Text>
 
       <TextInput
+        testID="new-skill-input"
         style={styles.input}
         placeholder={"New skill"}
         placeholderTextColor={"#555"}
         onChangeText={setNewSkill}
       />
 
-      <Button title={"Add"} onPress={handleAddNewSkill} />
-
-      <Text style={[styles.title, styles.spacing]}>My Skills</Text>
-
-      <FlatList
-        data={mySkills}
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <SkillCard
-            skill={item.name}
-            onPress={() => handleRemoveSkill(item.id)}
-          />
-        )}
+      <Button
+        testID="new-skill-button"
+        title={"Add"}
+        onPress={handleAddNewSkill}
       />
+
+      {mySkills.length > 0 && (
+        <Text style={[styles.title, styles.spacing]}>My Skills</Text>
+      )}
+
+      {mySkills.length > 0 && (
+        <FlatList
+          testID="flat-list-skills"
+          data={mySkills}
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="never"
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <SkillCard
+              testID={item.name}
+              skill={item.name}
+              onPress={() => handleRemoveSkill(item.id)}
+            />
+          )}
+        />
+      )}
     </View>
   );
 };
